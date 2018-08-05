20e90a1441f8655b2f9a5764066fc9ea25facf99
Nicholas Kimball <31974139+NKimball1@users.noreply.github.com>
2018-08-05 15:06:23 -0500

    Add files via upload

---
+++ PassGen.java
@@ -0,0 +1,22 @@
+
+public class PassGen {
+
+	public static void main(String[] args) {
+		
+		//Choose password length
+		int length; 
+		//Generate and return password
+		System.out.print("Your random password is: " + generate(length));
+		
+
+	}
+
+	static char[] generate(int length) {
+		
+		
+		return;
+	}
+	
+}
+
+
