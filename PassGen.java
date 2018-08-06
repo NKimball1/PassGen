55a979472923eff9af5da68b0a8e8a85365a2c4e
Nicholas Kimball <31974139+NKimball1@users.noreply.github.com>
2018-08-06 12:28:48 -0500

    Add files via upload

--- PassGen.java
+++ PassGen.java
@@ -1,20 +1,38 @@
 
+import java.util.*;
+
 public class PassGen {
 
+	 //Legal characters for generated password. 
+	static final String legalValues = 
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	
 	public static void main(String[] args) {
 		
-		//Choose password length
-		int length; 
+		//Password length set here
+		int length = 8; 
 		//Generate and return password
-		System.out.print("Your random password is: " + generate(length));
-		
+		System.out.println("Your random password is: ");
+		System.out.println(generate(length));
+	
 
 	}
-
+	/*
+	 * Method to generate password
+	 * 
+	 * Static; no specific instance necessary
+	 */
 	static char[] generate(int length) {
 		
+		char[] password = new char[length];
 		
-		return;
+		//Construct generator, very likely to have distinct seed
+		Random rng = new Random();
+		
+		for (int i = 0; i < length; i++) {
+			password[i] = legalValues.charAt(rng.nextInt(legalValues.length()));
+		}
+		return password;
 	}
 	
 }
